c910d8b7922ee75e5f2b04dd358d5da6eae6b475
koko1313 <kaloyanvellichkov@gmail.com>
2020-01-26 13:14:41 +0200

    added router and navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,52 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import Header from './components/Header';
+
+const Layout = props => (
+  <>
+      <Header/>
+      <div className="container mt-5">
+          {props.children}
+      </div>
+  </>
+);
+
+const routes = [
+  {
+      path: '/',
+      exact: true,
+      main: () => <Layout>
+          asd
+      </Layout>
+  },
+  {
+    path: '/gallery',
+    exact: true,
+    main: () => <Layout>
+        ga
+    </Layout>
+  },
+];
+
+const getRoutes = () => {
+  return routes.map((route, index) => {
+      return <Route key={index} exact={route.exact} path={route.path}>
+          {route.main}
+      </Route>;
+  });
+}
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <Switch>
+        {getRoutes()}
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/components/Header/index.js
@@ -0,0 +1,56 @@
+import React, {Component} from "react";
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    Nav,
+    NavItem,
+    NavLink
+} from 'reactstrap';
+
+import {NavLink as RRNavLink} from 'react-router-dom';
+import Container from "../../../node_modules/reactstrap/lib/Container";
+
+class Header extends Component {
+    
+    constructor(props) {
+        super(props);
+        this.state = {
+            isOpen: false
+        }
+    }
+
+    toggleIsOpen = () => {
+        this.setState({
+            isOpen: !this.state.isOpen
+        });
+    }
+
+    render() {
+        return <Navbar color="primary" dark expand="md">
+            <Container>
+                <NavLink
+                    tag={RRNavLink}
+                    className="navbar-brand"
+                    exact to="/">
+                    <i className="fa fa-cubes mr-3"></i>
+                    <span className="project-name">Software Technologies</span>
+                </NavLink>
+                <NavbarToggler onClick={this.toggleIsOpen} />
+                <Collapse isOpen={this.state.isOpen} navbar>
+                    <Nav className="ml-auto" navbar>
+                        <NavItem>
+                            <NavLink tag={RRNavLink} exact to="/" activeClassName="active">Начало</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink tag={RRNavLink} exact to="/gallery" activeClassName="active">Gallery</NavLink>
+                        </NavItem>
+                    </Nav>
+                </Collapse>
+            </Container>
+        </Navbar>
+    }
+
+}
+
+export default Header;
