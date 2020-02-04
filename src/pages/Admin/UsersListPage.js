f981e99b52f4fa52672b4d32efd9af9017727cb5
koko1313 <kaloyanvellichkov@gmail.com>
2020-02-04 12:10:23 +0200

    users list page

--- src/pages/Admin/UsersListPage.js
+++ src/pages/Admin/UsersListPage.js
@@ -1,19 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import networkClient from '../../network/network-client';
 
 const UsersListPage = () => {
 
-    const renderUsersList = () => {
+    const [users, setUsers] = useState();
+
+    useEffect(()=> {
         networkClient.get('/user/all', null, (users) => {
-            // TODO
-            console.log(users);
+            setUsers(users);
+        });
+    }, []);
+    
+    const renderUsersList = () => {
+        if(!users) return null;
+
+        return users.map((user) => {
+            return (
+                <tr key={user.id}>
+                    <td>{`${user.firstName} ${user.secondName} ${user.lastName}`}</td>
+                    <td>{user.email}</td>
+                </tr>
+            );
         });
     }
 
     return (
         <>
             <h1>Потребители</h1>
-            {renderUsersList()}
+
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th scope="col">Име</th>
+                        <th scope="col">Email</th>
+                        <th scope="col">Пол</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {renderUsersList()}
+                </tbody>
+            </table>
         </>
     );
 
