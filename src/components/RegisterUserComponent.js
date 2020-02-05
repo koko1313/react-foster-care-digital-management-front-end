5437ec4a8f06c46625ea8850938951b43a7a66d5
koko1313 <kaloyanvellichkov@gmail.com>
2020-02-06 00:10:59 +0200

    update user

--- src/components/RegisterUserComponent.js
+++ src/components/RegisterUserComponent.js
@@ -7,25 +7,51 @@ import SubRegionsSelect from './base-components/Form/Select/SubRegionsSelect';
 import CitiesSelect from './base-components/Form/Select/CitiesSelect';
 import PositionsSelect from './base-components/Form/Select/PositionsSelect';
 import { useParams } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import * as actions from '../redux/actions';
+import Loader from './base-components/Loader';
 
 const RegisterUserComponent = () => {
 
     const [alert, setAlert] = useState({color: null, message: null});
+    const isLoading = useSelector(state => state.loading);
 
-    const [position, setPosition] = useState();
-    const [email, setEmail] = useState();
-    const [password, setPassword] = useState();
-    const [rePassword, setRePassword] = useState();
-    const [firstName, setFirstName] = useState();
-    const [secondName, setSecondName] = useState();
-    const [lastName, setLastName] = useState();
-    const [region, setRegion] = useState();
-    const [subRegion, setSubRegion] = useState();
-    const [city, setCity] = useState();
+    const [isEditingUser, setIsEditingUser] = useState(false);
+
+    const [position, setPosition] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [rePassword, setRePassword] = useState("");
+    const [firstName, setFirstName] = useState("");
+    const [secondName, setSecondName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [region, setRegion] = useState("");
+    const [subRegion, setSubRegion] = useState("");
+    const [city, setCity] = useState("");
 
     const { id } = useParams(); // get parameter from url
+    const dispatch = useDispatch();
 
-    console.log(id); // TODO
+    useState(() => {
+        if(id) {
+            setIsEditingUser(true);
+            dispatch(actions.setLoading(true));
+
+            networkClient.get(`user/${id}`, null, (user) => {
+                console.log(user);
+                setPosition(user.position.id);
+                setEmail(user.email);
+                setFirstName(user.first_name);
+                setSecondName(user.second_name);
+                setLastName(user.last_name);
+                setRegion(user.region.id);
+                setSubRegion(user.sub_region.id);
+                setCity(user.city.id);
+                dispatch(actions.setLoading(false));
+            });
+        }
+    }, [id]);
+    
 
     const registerUser = () => {
         if(password !== rePassword) {
@@ -72,6 +98,50 @@ const RegisterUserComponent = () => {
         );
     }
 
+    const updateUser = () => {
+        if(password !== rePassword) {
+            setAlert({color: "danger", message: "Паролите не съвпадат!"});
+            return null;
+        }
+
+        networkClient.put(`/user/update/${id}`, 
+            {
+                email: email,
+                firstName: firstName,
+                secondName: secondName,
+                lastName: lastName,
+                regionId: region,
+                subRegionId: subRegion,
+                cityId: city,
+                positionId: position,
+            },
+            // success
+            (response) => {
+                setAlert({color: "success", message: "Успешно редактиран потребител!"});
+            },
+            // error
+            (error) => {
+                if(error.response) {
+                    switch(error.response.status) {
+                        case 409: {
+                            setAlert({color: "danger", message: "Вече съществува потребител с този email адрес!"});
+                            break;
+                        }
+                        case 400: {
+                            setAlert({color: "danger", message: "Не са попълнени всички полета!"});
+                            break;
+                        }
+                        default: {
+                            setAlert({color: "danger", message: "Възникна грешка!"});
+                        }
+                    }
+                } else {
+                    setAlert({color: "danger", message: "Възникна грешка!"});
+                }
+            }
+        );
+    }
+
     return (
         <>
             <Alert color={alert.color} message={alert.message} /> 
@@ -81,9 +151,12 @@ const RegisterUserComponent = () => {
 
                 <Input id="email" label="Email" type="email" placeholder="Email ..." onChange={(e) => setEmail(e.target.value)} value={email} />
 
-                <Input id="password" label="Парола" type="password" placeholder="Парола ..." onChange={(e) => setPassword(e.target.value)} />
-
-                <Input id="rePassword" label="Повтори парола" type="password" placeholder="Повтори парола ..." onChange={(e) => setRePassword(e.target.value)} />
+                {isEditingUser ? null :
+                    <>
+                        <Input id="password" label="Парола" type="password" placeholder="Парола ..." onChange={(e) => setPassword(e.target.value)} />
+                        <Input id="rePassword" label="Повтори парола" type="password" placeholder="Повтори парола ..." onChange={(e) => setRePassword(e.target.value)} />
+                    </>
+                }
 
                 <Input id="firstName" label="Име" type="text" placeholder="Име ..." onChange={(e) => setFirstName(e.target.value)} value={firstName} />
 
@@ -97,8 +170,14 @@ const RegisterUserComponent = () => {
 
                 <CitiesSelect id="city" label="Град" placeholder="Избери град ..." onChange={(e) => setCity(e.target.value)} value={city} />
 
-                <button type="button" className="btn btn-primary" onClick={registerUser}>Регистрирай</button>
+                {isEditingUser ?
+                    <button type="button" className="btn btn-warning" onClick={updateUser}>Редактирай</button>
+                    :
+                    <button type="button" className="btn btn-primary" onClick={registerUser}>Регистрирай</button>
+                }
             </form>
+
+            <Loader loading={isLoading} fullScreen={true} />
         </>
     );
 
--- src/components/base-components/Loader/index.js
+++ src/components/base-components/Loader/index.js
@@ -1,13 +1,40 @@
 import React from 'react';
 import ScaleLoader from "react-spinners/ScaleLoader";
-import './style.scss';
+import { css } from "@emotion/core";
 
 const Loader = (props) => {
 
+    const isFullScreen = () => {
+        if(props.fullScreen) {
+            return "full-screen";
+        }
+    };
+
+    const overrideCss = `
+        text-align: center;
+    `;
+
+    const fullScreenCss = `
+        background-color: rgba(90, 90, 90, 0.3);
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        padding: 50vh 0;
+    `;
+
+    const override = css`
+        ${overrideCss}
+        ${props.fullScreen ? fullScreenCss : null}
+    `;
+
     return (
-        <div className="loader">
-            <ScaleLoader loading={props.loading} />
-        </div>
+        <ScaleLoader 
+            loading={props.loading} 
+            css={override} 
+            color={"#6A89FD"} 
+        />
     );
 
 }
