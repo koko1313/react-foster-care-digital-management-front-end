614a958f29561c33334a4c42cbc7cde273cf38a9
koko1313 <kaloyanvellichkov@gmail.com>
2020-02-12 00:22:02 +0200

    show all family info on info page

--- src/components/Family/DetailsComponent/index.js
+++ src/components/Family/DetailsComponent/index.js
@@ -91,47 +91,121 @@ const DetailsComponent = () => {
 
         return (
             <>
+                {/* Жена */}
                 {family.woman &&
-                    <div className="row info">
-                        <div className="col-md-3 info-header">Жена</div>
-                        <div className="col info-body">
-                            {family.woman.first_name} {family.woman.second_name} {family.woman.last_name}
-                            {family.titular === "woman" && " (титуляр)"}
+                    <div className="info-section">
+                        <div className="row">
+                            <div className="col-md-3 info-header info-heading">Жена</div>
+                            <div className="col info-body"></div>
+                        </div>
+
+                        <div className="row">
+                            <div className="col-md-3 info-header">Име</div>
+                            <div className="col info-body">
+                                {family.woman.first_name} {family.woman.second_name} {family.woman.last_name}
+                                {family.titular === "woman" && " (титуляр)"}
+                            </div>
+                        </div>
+
+                        <div className="row">
+                            <div className="col-md-3 info-header">ЕГН</div>
+                            <div className="col info-body">{family.woman.egn}</div>
+                        </div>
+
+                        <div className="row">
+                            <div className="col-md-3 info-header">Телефон</div>
+                            <div className="col info-body">{family.woman.phone}</div>
+                        </div>
+
+                        <div className="row">
+                            <div className="col-md-3 info-header">Образование</div>
+                            <div className="col info-body">{family.woman.education}</div>
+                        </div>
+
+                        <div className="row">
+                            <div className="col-md-3 info-header">Месторабота</div>
+                            <div className="col info-body">{family.woman.work}</div>
                         </div>
                     </div>
                 }
 
+                {/* Мъж */}
                 {family.man &&
-                    <div className="row info">
-                        <div className="col-md-3 info-header">Мъж</div>
-                        <div className="col info-body">
-                            {family.man.first_name} {family.man.second_name} {family.man.last_name}
-                            {family.titular === "man" && " (титуляр)"}
+                    <div className="info-section">
+                        <div className="row">
+                            <div className="col-md-3 info-header info-heading">Мъж</div>
+                            <div className="col info-body"></div>
+                        </div>
+
+                        <div className="row">
+                            <div className="col-md-3 info-header info-header">Име</div>
+                            <div className="col info-body">
+                                {family.man.first_name} {family.man.second_name} {family.man.last_name}
+                                {family.titular === "man" && " (титуляр)"}
+                            </div>
+                        </div>
+
+                        <div className="row">
+                            <div className="col-md-3 info-header">ЕГН</div>
+                            <div className="col info-body">{family.man.egn}</div>
+                        </div>
+
+                        <div className="row">
+                            <div className="col-md-3 info-header">Телефон</div>
+                            <div className="col info-body">{family.man.phone}</div>
+                        </div>
+
+                        <div className="row">
+                            <div className="col-md-3 info-header">Образование</div>
+                            <div className="col info-body">{family.man.education}</div>
+                        </div>
+
+                        <div className="row">
+                            <div className="col-md-3 info-header">Месторабота</div>
+                            <div className="col info-body">{family.man.work}</div>
                         </div>
                     </div>
                 }
-                
-                <div className="row info">
-                    <div className="col-md-3 info-header">Предпочитан пол на децата</div>
-                    <div className="col info-body">
-                        {family.prefer_kid_gender === "m" && "Момче"}
-                        {family.prefer_kid_gender === "f" && "Момиче"}
+
+                {/* Предпочитания към децата */}
+                <div className="info-section">
+                    <div className="row">
+                        <div className="col-md-3 info-header info-heading">Предпочитания към деца</div>
+                        <div className="col info-body"></div>
+                    </div>
+
+                    <div className="row">
+                        <div className="col-md-3 info-header">Предпочитан пол</div>
+                        <div className="col info-body">
+                            {family.prefer_kid_gender === "m" && "Момче"}
+                            {family.prefer_kid_gender === "f" && "Момиче"}
+                        </div>
+                    </div>
+
+                    <div className="row">
+                        <div className="col-md-3 info-header">Предпочитана възраст</div>
+                        <div className="col info-body">
+                            Минимална: {family.prefer_kid_min_age} <br />
+                            Максимална: {family.prefer_kid_max_age}
+                        </div>
                     </div>
                 </div>
-                <div className="row info">
-                    <div className="col-md-3 info-header">Предпочитана възраст на децата</div>
-                    <div className="col info-body">
-                        Минимална: {family.prefer_kid_min_age} <br />
-                        Максимална: {family.prefer_kid_max_age}
+
+                {/* За семейството */}
+                <div className="info-section">
+                    <div className="row">
+                        <div className="col-md-3 info-header info-heading">За семейството</div>
+                        <div className="col info-body"></div>
                     </div>
-                </div>
-                <div className="row info">
-                    <div className="col-md-3 info-header">Адрес</div>
-                    <div className="col info-body">
-                        {family.region && family.region.name + ", "}
-                        {family.sub_region && family.sub_region.name + ", "}
-                        {family.city && family.city.name + ", "} 
-                        {family.address}
+
+                    <div className="row">
+                        <div className="col-md-3 info-header">Адрес</div>
+                        <div className="col info-body">
+                            {family.region && family.region.name + ", "}
+                            {family.sub_region && family.sub_region.name + ", "}
+                            {family.city && family.city.name + ", "} 
+                            {family.address}
+                        </div>
                     </div>
                 </div>
 
