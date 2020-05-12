50f72eb40bca0f1d72f1871349831e0bb6f36b7c
koko1313 <kaloyanvellichkov@gmail.com>
2020-05-12 23:51:57 +0300

    added some semicolons

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,4 +1,4 @@
 /// <reference types="react-scripts" />
-declare module 'react-redux'
-declare module 'react-router-dom'
+declare module 'react-redux';
+declare module 'react-router-dom';
 declare module 'reactstrap';
